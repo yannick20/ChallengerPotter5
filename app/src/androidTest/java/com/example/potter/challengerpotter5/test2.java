f45d66c4b6e558c5210cf8e310e4bc838224ff7c
yannick20 <37401491+yannick20@users.noreply.github.com>
2018-03-29 22:48:48 +0100

    version_2

---
+++ app/src/androidTest/java/com/example/potter/challengerpotter5/test2.java
@@ -0,0 +1,61 @@
+package com.example.potter.challengerpotter5;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.Assert.assertNotNull;
+
+/**
+ * Created by Potter on 27/03/2018.
+ */
+
+public class test2 {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+    private MainActivity mainActivity=null;
+
+    @Before
+    public void setUp() throws Exception {
+        mainActivity = mainActivityActivityTestRule.getActivity();
+
+    }
+
+    @Test
+    public void name() throws Exception {
+        assertNotNull(mainActivity.findViewById(R.id.txt_mail));
+        assertNotNull(mainActivity.findViewById(R.id.txt_pwd));
+        assertNotNull(mainActivity.findViewById(R.id.btn_login));
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+        onView(withId(R.id.txt_mail)).perform(typeText("ghost")).toString();
+
+        onView(withId(R.id.txt_pwd)).perform(typeText("1234"));
+
+        onView(withId(R.id.btn_login)).perform(click());
+
+        onView(withId(R.id.txt_mail)).perform(clearText());
+
+        onView(withId(R.id.txt_pwd)).perform(clearText());
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+        pressBack();
+    }
+
+
+
+    @After
+    public void tearDown() throws Exception {
+        mainActivity=null;
+    }
+}
